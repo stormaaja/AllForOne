fdb711304526b76ad698194de8dfbc61ee14f6c0
Matti Ahinko <matti@ratassoftware.fi>
2015-01-24 18:03:07 +0200

    Add license

--- public/js/network.js
+++ public/js/network.js
@@ -1,3 +1,15 @@
+/*
+ *
+ * All for One (c) by FGJ15 JKL Team for All for One
+ *
+ * All for One is licensed under a
+ * Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * 
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-sa/3.0/>.
+ *
+*/
+
 function Network() {
 
 	this.onConnected = function() {};
