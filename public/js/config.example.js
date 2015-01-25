0ec027394e57ff4307480c954d60524abf6a9d27
Matti Ahinko <matti@ratassoftware.fi>
2015-01-25 11:23:07 +0200

    Add example config file

---
+++ public/js/config.example.js
@@ -0,0 +1,15 @@
+/*
+ *
+ * All for One (c) by FGJ15 JKL Team for All for One
+ *
+ * All for One is licensed under a
+ * Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * 
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-sa/3.0/>.
+ *
+ * This is example config file. Copy this file to config.js
+ * and set correct server address.
+ *
+*/
+window.server = "http://1.270.0.1:1233";
