82dd513c5e62196771a8b13e6f9f45cced6af68b
Matti Ahinko <matti@ratassoftware.fi>
2015-01-25 10:16:49 +0200

    Add license and some instructions

--- config/config.example.rb
+++ config/config.example.rb
@@ -1,3 +1,14 @@
+#
+# All for One (c) by FGJ15 JKL Team for All for One
+# 
+# All for One is licensed under a
+# Creative Commons Attribution-ShareAlike 3.0 Unported License.
+# 
+# You should have received a copy of the license along with this
+# work.  If not, see <http://creativecommons.org/licenses/by-sa/3.0/>.
+# 
+# Copy this file to config.rb and make correct adjustments
+#
 set :bind, '1.270.0.1'
 set :port, 1233
 set :environment, :production
